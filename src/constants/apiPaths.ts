a186ec54a70378620b3b5210a3303efc0b3eb399
Oleg Konan <olegkonanff@gmail.com>
2025-02-21 22:54:12 +0400

    fix(frontend): :alien: api path for products

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://3b47j6o3bf.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
