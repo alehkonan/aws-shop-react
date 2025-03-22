3d1b44dd0e2c85efd8d6fb574b0f8258a4a38e36
Oleg Konan <olegkonanff@gmail.com>
2025-03-22 09:32:08 +0400

    chore(frontend): :alien: change url for product api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://3b47j6o3bf.execute-api.us-east-1.amazonaws.com/dev",
+  product: "https://v8c9svg7lk.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://kr7h9842ek.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
