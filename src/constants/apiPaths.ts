3626544657892150157b7ce5ebe3459dfccabdcb
Oleg Konan <olegkonanff@gmail.com>
2025-04-11 12:00:14 +0400

    chore(frontend): :alien: update order and cart api paths

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://v8c9svg7lk.execute-api.us-east-1.amazonaws.com/dev",
-  order: "https://cb5p50g3jb.execute-api.us-east-1.amazonaws.com/dev",
+  order: "https://1s6jrf27j8.execute-api.us-east-1.amazonaws.com/dev",
   import: "https://kr7h9842ek.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://cb5p50g3jb.execute-api.us-east-1.amazonaws.com/dev",
+  cart: "https://1s6jrf27j8.execute-api.us-east-1.amazonaws.com/dev",
 } as const;
 
 export default API_PATHS;
